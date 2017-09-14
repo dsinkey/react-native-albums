efa3bcb3bf5041a0126c3b9570b803e554685982
Dustin Sinkey <dustinsinkey@Vittorias-MacBook-Pro.local>
2017-09-13 21:10:01 -0700

    Added HeaderList Component and sample JSON data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
@@ -22,8 +22,10 @@ const styles = {
       height: 60,
       paddingTop: 15,
       shadowColor: '#000',
-      shadowOffset: {width: 0, height: 2},
-      shadowOpacity: .2
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative'
     }
 };
 
