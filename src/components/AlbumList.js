6e288c7caeadf2abd0a92c677d887cd4169e9b11
Dustin Sinkey <dustinsinkey@Vittorias-MacBook-Pro.local>
2017-09-14 20:36:54 -0700

    Created Card, Card Section, and AlbumDetail Component and added them to the AlbumList Component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://raw.githubusercontent.com/dsinkey/react-native-albums/master/sample-data.json')
+    .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail album={album} key={album.title} />
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
