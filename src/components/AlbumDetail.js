523bd1304e4323168f8acfc48896d2a19f6e7506
Dustin Sinkey <dustinsinkey1@gmail.com>
2017-09-16 16:47:06 -0700

    Added more to AlbumDetail, and styled it

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const {title, artist, thumbnail_image, image} = album;
+  const { headerContentStyle,
+          thumbnailStyle,
+          thumbnailContainerStyle,
+          headerTextStyle,
+          imageStyle } = styles;
+
   return (
       <Card>
         <CardSection>
-          <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={ headerTextStyle }>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+        </CardSection>
+        <CardSection>
+          <Image style={ imageStyle } source={{ uri: image }}>
+          </Image>
         </CardSection>
       </Card>
   )
 };
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  imageStyle: {
+    height: 300,
+    width: null,
+    flex: 1
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
+}
 
 export default AlbumDetail;
