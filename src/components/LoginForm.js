e2514e44e65adecf62dc8cac21d26f9a61e778b2
Dustin Sinkey <dustinsinkey1@gmail.com>
2017-09-17 19:27:39 -0700

    Added resuable Input component and added it to the LoginForm Component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createWithEmailAndPassword(email, password)
+        .catch(() => {
+          this.setState({ error: 'Authentication Failed.' });
+        });
+      });
+  }
+
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
-        <CardSection >
-         <Button>Log In</Button>
-        </CardSection >
+        <CardSection>
+         <Input
+          placeholder="user@gmail.com"
+          label="Email"
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+         />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <Text>{this.state.error}</Text>
+
+        <CardSection>
+         <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
+        </CardSection>
       </Card>
     );
   }
